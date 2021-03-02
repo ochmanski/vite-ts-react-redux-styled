26fc2bbc0325d09310a6d3a4e6ab8bb42d4c97ab
ochdev <contact@och.dev>
2021-03-02 19:59:47 +0100

    Initial

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,25 @@
+import { query } from 'atomic-layout';
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  @media ${query({ to: { minWidth: '764px' } })} {
+    :root {
+      font-size: 90%;
+    }
+  }
+`;
